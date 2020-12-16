243226cc349fd9a38c335d7bfecbdb7d915df195
Mayank <thedeveloper.mayank@gmail.com>
2020-12-16 20:59:11 +0530

    updated chat.js

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,24 @@
 const socket = io()
 
+// * Elements
+const $messageForm = document.querySelector("#message-form")
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+
+
 //  Here `message` is a parameter which we,
 //  client 'emits' to the server
 //  and 'sendMessage' is the event name.
 
-const messageForm = document.querySelector("#message-form")
-messageForm.addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    // Disabling the form
     const message = document.querySelector("#enterMessage").value
+
     socket.emit('sendMessage', message, (error) => {
+        // Enabling the form
+
         if (error) {
             return console.log(error)
         }
